3198b48b575162ec9df266926b3b27cfa92844b2
MrJeleika <den932005@gmail.com>
2022-06-12 11:09:50 +0300

    1 task

---
+++ js1.js
@@ -0,0 +1,9 @@
+let camelCase = 0;
+let snake_case = 1;
+let PascalCase = 2;
+console.log(camelCase,snake_case,PascalCase);
+alert(camelCase)
+alert(snake_case)
+alert(PascalCase)
+
+// alert выводится в модальном окне на странице, а console.log в консоль разработчика
