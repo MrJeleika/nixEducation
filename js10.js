fd736fe464c1da193201ee873f194155544a1d2e
MrJeleika <den932005@gmail.com>
2022-06-12 13:46:19 +0300

    10 task

---
+++ js10.js
@@ -0,0 +1,10 @@
+let deleteUnwantedNames = function (arr) {
+  const unwantedNames = ['Дима', 'Саша', 'Ольга','Никита'];
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < unwantedNames.length;j++){
+      if(arr[i] == unwantedNames[j]) arr.splice(i,1)
+    }
+  }
+  return arr
+}
+console.log(deleteUnwantedNames(['Саша','Ольга', 'Никита', 'Анастасия', 'Дима', 'Саныч']));
