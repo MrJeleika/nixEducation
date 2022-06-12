2b23f8160cccb28cec2cbb14e5cd66ad10f25071
MrJeleika <den932005@gmail.com>
2022-06-12 11:34:28 +0300

    5 task

---
+++ js5.js
@@ -0,0 +1,11 @@
+let num1 = prompt("Первое число");
+let num2 = prompt("Второе число");
+if (num1 > num2) {
+  alert(+num1 + +num2)
+}
+if (num1 < num2) {
+  alert(+num1 * +num2)
+}
+if(num1 === num2){
+  alert('Числа одинаковые')
+}
