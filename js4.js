e2af21c54af45209729bdd817b156625b5929f47
MrJeleika <den932005@gmail.com>
2022-06-12 11:26:49 +0300

    4 task

---
+++ js4.js
@@ -0,0 +1,10 @@
+// Первый способ
+let arr1 = ['Саша', 'Петя', 'Оксана'];
+arr1.reverse();
+arr1[1] = 'Олег';
+console.log(arr1);
+// Второй способ
+let arr2 = ['Саша', 'Петя', 'Оксана'];
+let arr3 = arr2.reverse()
+arr3[1] = 'Олег';
+console.log(arr3);
