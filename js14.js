756e1d9562d17c9058b2e9b9f0a1ab84f7df6136
MrJeleika <den932005@gmail.com>
2022-06-12 16:57:08 +0300

    14 task

---
+++ js14.js
@@ -0,0 +1,15 @@
+let remove = function(str, n){
+  
+
+  for(let i = 0; i < str.length; i++){
+    if(str[i] == "!") {
+      str = str.split('')
+      str.splice(i,1)
+      str = str.join('');
+      n -= 1;
+    }
+    if(n == 0) return str
+  }
+  
+}
+console.log(remove("Hi!!", 1));
